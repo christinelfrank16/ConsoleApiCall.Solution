a9971645803007b7b073ce62c068be76af4fc103
christopher burge and christine frank <christopher@>
2019-10-29 09:56:45 -0700

    initial file setup.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using ConsoleApiCall.Models;
+
+namespace ApiTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DotNetEnv.Env.Load();
+      string key = System.Environment.GetEnvironmentVariable("API_KEY");
+      var apiCallTask = ApiHelper.ApiCall(key);
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+      foreach (Article article in articleList)
+            {
+                Console.WriteLine($"Section: {article.Section}");
+                Console.WriteLine($"Title: {article.Title}");
+                Console.WriteLine($"Abstract: {article.Abstract}");
+                Console.WriteLine($"Url: {article.Url}");
+                Console.WriteLine($"Byline: {article.Byline}");
+            }
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
